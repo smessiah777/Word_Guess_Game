4d6b9ce444936172496520f8d100baad0a8cd9c4
roninTDK <smessiah17@gmail.com>
2019-03-03 17:00:02 -0600

    built a game start button that calls a function to chooses random word from array of game words and replaces and displays the game words as blank spaces on to the webpage. Still need to write code to capture user input, inorder to complete other functionality of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,7 @@
 window.onload = function() {
-  var gameWords = [
-    "lone wolf",
-    "cub",
-    "the shogun",
-    "lord bizen",
-    "the supreme ninja"
-  ];
+  var gameWords = ["ronin", "cub", "shogun", "bizen", "ninja"]; //core game words
 
-  var wrong = []; //letters guessed wrong
+  var wordArray = []; //empty array to populate key inputs
   var rightLetters = 0; //letters guessed correctly that match game words
   var score = 0; // games won words guessed correctly
   var guessesLeft = 10; //guesses allowed guesses remaining
@@ -15,39 +9,70 @@ window.onload = function() {
   var dash = 0; // for spaces in words
 
   // get elements
-  var displayRight = document.getElementById("correct-letter");
-  var displayWrong = document.getElementById("incorrect-letter");
   var displayGuessesLeft = document.getElementById("guesses-remaining");
+  var statusInfo = document.getElementById("prompt");
+  var displayInput = document.getElementById("word-display");
   var displayScore = document.getElementById("score");
   var displayImg = document.getElementById("game-img");
 
-  ///Game Start
+  // Game Start
   document.getElementById("start").addEventListener("click", wordSelect);
 
   //Display Guesses Left
-  function livesLeft() {
-    displayGuessesLeft.innerHTML = guessesLeft;
-    if (guessesLeft < 1) {
-      displayGuessesLeft.innerHTML = "You Lose! The Shogun Lives!";
-    }
-    for (var i = 0; i < wrong.length; i++) {
-      if (rightLetters === wrong.length) {
-        displayGuessesLeft.innerHTML = "You Win! The Shogun is Slain!";
-      }
-    }
-  }
 
-  livesLeft();
+  displayGuessesLeft.innerHTML = guessesLeft;
+  // if (guessesLeft < 1) {
+  //   displayGuessesLeft.innerHTML = "You Lose! The Shogun Lives!";
+  // }
+  // for (var i = 0; i < wrong.length; i++) {
+  //   if (rightLetters === wrong.length) {
+  //     displayGuessesLeft.innerHTML = "You Win! The Shogun is Slain!";
+  //   }
+  // }
 
   //Generate Random Game Words from Array
+
   //game words randomly selected
   function wordSelect() {
     var currentWord = gameWords[Math.floor(Math.random() * gameWords.length)];
-    console.log(currentWord);
+    ///
+    for (var i = 0; i < currentWord.length; i++) {
+      wordArray[i] = "_";
+    }
+
+    var letters = currentWord.length;
+
+    /////
+    displayInput.innerHTML = wordArray;
+
+    // while (letters > 0) {
+    //   //  document.getElementById("word-display");
+    //   console.log(wordArray.join(" "));
+
+    //   document.onkeyup = function(event) {
+    //     displayInput.textContent = event.key;
+    //   };
+
+    //   var statusText = (statusInfo.innerHTML =
+    //     "GUESS A LETTER WITH YOUR KEYBOARD");
+    //   if (displayInput === null) {
+    //     break;
+    //   } else if (displayInput.length !== 1) {
+    //     statusText = statusInfo.innerHTML = "CHOOSE A LETTER";
+    //   } else {
+    //     for (var j = 0; j < gameWords.length; j++) {
+    //       if (word[j] === keyGuess) {
+    //         wordArray[j] = displayInput;
+    //         letters--;
+    //       }
+    //     }
+    //   }
+    // }
   }
 
-  //match key input against gameWords array for right guesses
-  document.onkeyup = function(event) {
-    console.log(event.key);
-  };
+  // var letters = wordSelect;
+
+  //array to act as place holder for empty letter spaces
+
+  ///Loop to update the game and show user the necessary info
 };
